c3b6e55a50e71bfdfb0fbc938f752f233e718fae
Ariele Fatima <ariele_afd@yahoo.com.br>
2019-08-31 13:38:37 -0300

    implementaçao de crud e arquivo de configuracao

--- GJJA.RegistraVoce.App.Console/Program.cs
+++ GJJA.RegistraVoce.App.Console/Program.cs
@@ -4,33 +4,107 @@ using GJJA.RegistraVoce.DataAccess.DAOs;
 using GJJA.RegistraVoce.Domain;
 using System.Collections.Generic;
 using GJJA.RegistraVoce.Domain.Enums;
+using Microsoft.Extensions.Configuration;
 
 namespace GJJA.RegistraVoce.App.Console
 {
     class Program
     {
+        private static string _connectionString = "";
         static void Main(string[] args)
         {
+            SetUp();
+            int opcao = -1;
             UI.WriteLine($"*** Bem Vindo *** {Environment.NewLine}");
-            UI.WriteLine($"O que você deseja fazer? {Environment.NewLine}");
-            UI.WriteLine("1. Listar pessoas");
-            UI.WriteLine("2. Inserir pessoa");
-            UI.WriteLine("0. Sair");
-            int opcao = Convert.ToInt32(UI.ReadLine());
-            switch(opcao)
+            while(opcao != 0)
             {
-                case 0:
-                    UI.WriteLine(" Tchau! ;)");
-                    break;
-                case 1:
-                    ShowPeople();
-                    break;
-                case 2:
-                    InsertPerson();
-                    break;
-                default:
-                    UI.WriteLine("Opção Inválida!");   
-                    break; 
+                UI.WriteLine($"O que você deseja fazer? {Environment.NewLine}");
+                UI.WriteLine("1. Listar pessoas");
+                UI.WriteLine("2. Inserir pessoa");
+                UI.WriteLine("3. Atualizar pessoa");
+                UI.WriteLine("4. Deletar pessoa");
+                UI.WriteLine("5. Contar pessoas");
+                UI.WriteLine("0. Sair");
+                opcao = Convert.ToInt32(UI.ReadLine());
+
+                switch(opcao)
+                {
+                    case 0:
+                        UI.WriteLine(" Tchau! ;)");
+                        break;
+                    case 1:
+                        ShowPeople();
+                        break;
+                    case 2:
+                        InsertPerson();
+                        break;
+                    case 3:
+                        UpdatePerson();
+                        break; 
+                    case 4:
+                        DeletePerson();
+                        break; 
+                    case 5:
+                        CountPeople();
+                        break;         
+                    default:
+                        UI.WriteLine("Opção Inválida!");   
+                        break; 
+                }
+            }
+        }
+
+        private static void CountPeople()
+        {
+            try
+            {
+                PersonDAO personDAO = new PersonDAO(_connectionString);
+                UI.WriteLine($"Existe(m) {personDAO.Count()} pessoa(s) cadastrada(s).");
+            }
+            catch(Exception ex)
+            {
+                UI.WriteLine($"Houve um erro ao contar a pessoa: {ex.Message}");
+            }
+        }
+
+
+        private static void DeletePerson()
+        {
+            try
+            {
+                ShowPeople();
+                UI.Write("Id da pessoa a ser deletada: ");
+                int personId = Convert.ToInt32(UI.ReadLine());
+                PersonDAO personDAO = new PersonDAO(_connectionString);
+                personDAO.Delete(personId);
+
+            }
+            catch(Exception ex)
+            {
+                UI.WriteLine($"Houve um erro ao deletar a pessoa: {ex.Message}");
+            }
+
+        }
+
+        private static void UpdatePerson()
+        {            
+            try
+            {
+                ShowPeople();
+                UI.Write("Id da pessoa a ser atualizada: ");
+                int personId = Convert.ToInt32(UI.ReadLine());
+                PersonDAO personDAO = new PersonDAO(_connectionString);
+                Person person = personDAO.SelectById(personId);
+                if( person == null)
+                {
+                    throw new ArgumentException("ID de pessoa inexistente.");
+                }
+                GetPersonFromUI(person);
+                personDAO.Update(person);
+            }
+            catch(Exception ex)
+            {
+                UI.WriteLine($"Houve um erro ao atualizar a pessoa: {ex.Message}");
             }
         }
 
@@ -39,28 +113,13 @@ namespace GJJA.RegistraVoce.App.Console
             try
             {
                 UI.WriteLine("*** Inserção de pessoa ***");
-                Person person =  new Person();
-                UI.Write(" - Nome: ");
-                person.Name = UI.ReadLine();
-                UI.Write(" - Gênero (0 = M, 1 = F, 2 = Indefinido): ");
-                person.Gender = (Gender)Convert.ToInt32(UI.ReadLine());
-                UI.Write(" - CPF: ");
-                person.DocumentNumber = UI.ReadLine();
-                UI.Write(" - RG: ");
-                person.Identification = UI.ReadLine();
-                UI.Write(" - Data de Nascimento: ");
-                person.BirthDate = Convert.ToDateTime(UI.ReadLine());
-                UI.Write(" - Estado civil (0 = Solt., 1 = Cas., 2 = Divorc., 3 = Viuv.)");
-                person.MaritalStatus = (MaritalStatus)Convert.ToInt32(UI.ReadLine());
-                UI.Write(" - Endereço: ");
-                person.Address = UI.ReadLine();
-                UI.Write(" - Telefone");
-                person.Phone = UI.ReadLine();
-                PersonDAO personDAO = new PersonDAO();
+                Person person = new Person();
+                GetPersonFromUI(person);
+                PersonDAO personDAO = new PersonDAO(_connectionString);
                 personDAO.Insert(person);
                 UI.WriteLine(" *** Pessoa cadastrado com sucesso! ** ");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 UI.WriteLine($"Houve um erro ao salvar a pessoa: {ex.Message}");
             }            
@@ -68,7 +127,7 @@ namespace GJJA.RegistraVoce.App.Console
 
         private static void ShowPeople()
         {
-            PersonDAO personDAO = new PersonDAO();
+            PersonDAO personDAO = new PersonDAO(_connectionString);
             List<Person> people = personDAO.Select();
             if(people.Count == 0)
             {
@@ -80,5 +139,56 @@ namespace GJJA.RegistraVoce.App.Console
                 UI.WriteLine($"[{person.Id}] {person.Name}");
             });
         }
+        private static void GetPersonFromUI(Person person)
+        {
+            if(string.IsNullOrEmpty(person.Name))
+            {
+                UI.Write(" - Nome: ");
+                person.Name = UI.ReadLine();
+                UI.Write(" - Gênero - 0 = M, 1 = F, 2 = Indefinido: ");
+                person.Gender = (Gender)Convert.ToInt32(UI.ReadLine());
+                UI.Write(" - CPF: ");
+                person.DocumentNumber = UI.ReadLine();
+                UI.Write(" - RG: ");
+                person.Identification = UI.ReadLine();
+                UI.Write(" - Data de Nascimento: ");
+                person.BirthDate = Convert.ToDateTime(UI.ReadLine());
+                UI.Write(" - Estado civil (0 = Solt., 1 = Cas., 2 = Divorc., 3 = Viuv.)");
+                person.MaritalStatus = (MaritalStatus)Convert.ToInt32(UI.ReadLine());
+                UI.Write(" - Endereço: ");
+                person.Address = UI.ReadLine();
+                UI.Write(" - Telefone: ");
+                person.Phone = UI.ReadLine();
+            }
+            else
+            {
+                UI.Write($" - Nome: ({person.Name}): ");
+                person.Name = UI.ReadLine();
+                UI.Write($" - Gênero - 0 = M, 1 = F, 2 = Indefinido ({person.Gender}): ");
+                person.Gender = (Gender)Convert.ToInt32(UI.ReadLine());
+                UI.Write($" - CPF ({person.DocumentNumber}): ");
+                person.DocumentNumber = UI.ReadLine();
+                UI.Write($" - RG ({person.Identification}): ");
+                person.Identification = UI.ReadLine();
+                UI.Write($" - Data de Nascimento ({person.BirthDate}): ");
+                person.BirthDate = Convert.ToDateTime(UI.ReadLine());
+                UI.Write($" - Estado civil - 0 = Solt., 1 = Cas., 2 = Divorc., 3 = Viuv. ({person.MaritalStatus}): ");
+                person.MaritalStatus = (MaritalStatus)Convert.ToInt32(UI.ReadLine());
+                UI.Write($" - Endereço ({person.Address}): ");
+                person.Address = UI.ReadLine();
+                UI.Write($" - Telefone ({person.Phone}): ");
+                person.Phone = UI.ReadLine();
+            }            
+                        
+        }
+
+        private static void SetUp()
+        {
+            //Configuration File
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            IConfigurationRoot config = configBuilder.Build();
+            _connectionString = config.GetConnectionString("MySqlConnStr");
+        }
+
     }
 }
--- GJJA.RegistraVoce.DataAcess/DAOs/PersonDAO.cs
+++ GJJA.RegistraVoce.DataAcess/DAOs/PersonDAO.cs
@@ -11,11 +11,17 @@ namespace GJJA.RegistraVoce.DataAccess.DAOs
 {
     public class PersonDAO
     {
+        private string _connectionString;
+        public PersonDAO(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
         public List<Person> Select()
         {
             List<Person> people = new List<Person>();
 
-            using(DbConnection conn = DbUtils.CreateConnection())
+            using(DbConnection conn = DbUtils.CreateConnection(_connectionString))
             using (DbCommand command = conn.CreateCommand())
             {                
                 command.CommandText = "SELECT * FROM pes_pessoas";
@@ -23,42 +29,111 @@ namespace GJJA.RegistraVoce.DataAccess.DAOs
                 DbDataReader reader = command.ExecuteReader();
                 while(reader.Read())
                 {
-                    people.Add(new Person
-                    {
-                        Id = Convert.ToInt32(reader["pes_id"]),
-                        Name = reader["pes_nome"].ToString(),
-                        Gender = (Gender)Convert.ToInt32(reader["pes_sexo"]),
-                        DocumentNumber = reader["pes_cpf"].ToString(),
-                        Identification = reader["pes_rg"].ToString(),
-                        BirthDate = Convert.ToDateTime(reader["pes_data_nascimento"].ToString()),
-                        MaritalStatus = (MaritalStatus)Convert.ToInt32(reader["pes_estado_civil"]),
-                        Address = reader["pes_endereco"].ToString(),
-                        Phone = reader["pes_telefone"].ToString(),
-                    });
-                }          
+                    people.Add(ExtractFromDataReader(reader));
+                }
                 return people;
             }
         }
 
+        public Person SelectById(int personId)
+        {
+            using(DbConnection conn = DbUtils.CreateConnection(_connectionString))
+            using (DbCommand command = conn.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM pes_pessoas WHERE pes_id = @pes_id";
+                command.SetParameter("@pes_id", personId);
+                DbDataReader reader = command.ExecuteReader();
+                if(reader.Read())
+                {
+                    return ExtractFromDataReader(reader);
+                }
+                return null;
+            }   
+        }
+
+
         public void Insert(Person person)
         {
-            using(DbConnection conn = DbUtils.CreateConnection())
+            using(DbConnection conn = DbUtils.CreateConnection(_connectionString))
             using(DbCommand command = conn.CreateCommand())
             {
                 command.CommandText = "INSERT INTO pes_pessoas (pes_nome, pes_sexo, pes_cpf, pes_rg, pes_data_nascimento, pes_estado_civil, pes_endereco, pes_telefone)" +
                 "VALUES (@pes_nome, @pes_sexo, @pes_cpf, @pes_rg, @pes_data_nascimento, @pes_estado_civil, @pes_endereco, @pes_telefone)";
-                command.SetParameter("@pes_nome", person.Name);
-                command.SetParameter("@pes_sexo", person.Gender);
-                command.SetParameter("@pes_cpf", person.DocumentNumber);
-                command.SetParameter("@pes_rg", person.Identification);
-                command.SetParameter("@pes_data_nascimento", person.BirthDate);
-                command.SetParameter("@pes_estado_civil", person.MaritalStatus);
-                command.SetParameter("@pes_endereco", person.Address);
-                command.SetParameter("@pes_telefone", person.Phone);
+                SetParameters(person, command);
                 command.ExecuteNonQuery();
-
             }
         }
+
+        public void Update(Person person)
+        {
+            using(DbConnection conn = DbUtils.CreateConnection(_connectionString))
+            using(DbCommand command = conn.CreateCommand())
+            {
+                command.CommandText = "UPDATE pes_pessoas SET " +
+                                      " pes_nome = @pes_nome, " +
+                                      " pes_sexo = @pes_sexo, " +
+                                      " pes_cpf = @pes_cpf, " +
+                                      " pes_rg = @pes_rg, " +
+                                      " pes_data_nascimento = @pes_data_nascimento, " +
+                                      " pes_estado_civil = @pes_estado_civil, " +
+                                      " pes_endereco = @pes_endereco, " +
+                                      " pes_telefone = @pes_telefone " +
+                                      " WHERE pes_id = @pes_id";
+                SetParameters(person, command);
+                command.SetParameter("@pes_id", person.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int personId)
+        {
+            using(DbConnection conn = DbUtils.CreateConnection(_connectionString))
+            using(DbCommand command = conn.CreateCommand())
+            {
+                command.CommandText = "DELETE FROM pes_pessoas WHERE pes_id = @pes_id";
+                command.SetParameter("@pes_id", personId);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public int Count()
+        {
+            using(DbConnection conn = DbUtils.CreateConnection(_connectionString))
+            using(DbCommand command = conn.CreateCommand())
+            {
+                command.CommandText = "SELECT COUNT(pes_id) FROM pes_pessoas";
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
+        private void SetParameters(Person person, DbCommand command)
+        {
+            command.SetParameter("@pes_nome", person.Name);
+            command.SetParameter("@pes_sexo", person.Gender);
+            command.SetParameter("@pes_cpf", person.DocumentNumber);
+            command.SetParameter("@pes_rg", person.Identification);
+            command.SetParameter("@pes_data_nascimento", person.BirthDate);
+            command.SetParameter("@pes_estado_civil", person.MaritalStatus);
+            command.SetParameter("@pes_endereco", person.Address);
+            command.SetParameter("@pes_telefone", person.Phone);
+        }
+
+        private static Person ExtractFromDataReader(DbDataReader reader)
+        {
+            return new Person
+            {
+                Id = Convert.ToInt32(reader["pes_id"]),
+                Name = reader["pes_nome"].ToString(),
+                Gender = (Gender)Convert.ToInt32(reader["pes_sexo"]),
+                DocumentNumber = reader["pes_cpf"].ToString(),
+                Identification = reader["pes_rg"].ToString(),
+                BirthDate = Convert.ToDateTime(reader["pes_data_nascimento"].ToString()),
+                MaritalStatus = (MaritalStatus)Convert.ToInt32(reader["pes_estado_civil"]),
+                Address = reader["pes_endereco"].ToString(),
+                Phone = reader["pes_telefone"].ToString(),
+            };
+        }
+
 
 
     }
